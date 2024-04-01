3f7c9d3d6b3ff03cb669c6a0377013009f40e632
jofaco <jofaco1916@hotmail.com>
2024-04-01 11:44:23 -0500

    cambios pequeños conteo palabras

--- BibliotecaEposters/urls.py
+++ BibliotecaEposters/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'BibliotecaEposters'
 urlpatterns = [
     # user
-    #path('list', views.getList, name='biblioteca_eposters_list'),
-    #path('list/<str:capitulo>', views.getListCapitulo, name='biblioteca_eposters_list'),
+    path('list', views.getList, name='biblioteca_eposters_list'),
+    path('list/<str:capitulo>', views.getListCapitulo, name='biblioteca_eposters_list'),
     #path('edit_User/<int:pk>/', UserUpdate.as_view(), name='user_update'),
     #path('delete_user/<int:pk>/', UserDeleteView.as_view(), name='user_delete'),
 ]
