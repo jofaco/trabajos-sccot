d2eec8c46f7ae611beb6b0990d95b7717a062fe7
jofaco <jofaco1916@hotmail.com>
2024-09-18 12:07:16 -0500

    se añade maquetacion chatia

--- Chatia/urls.py
+++ Chatia/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import hola_mundo
+from .views import PreguntarChatGPT,hola_mundo
 
 urlpatterns = [
-    #path('', PreguntarChatGPT.as_view(), name='preguntarChatGPT'),
+    path('', PreguntarChatGPT.as_view(), name='preguntarChatGPT'),
     path('hola-mundo/', hola_mundo, name='holaMundo'),
 ]
