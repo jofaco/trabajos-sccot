883854ebbfc40db6a5413e5016ae6e597368b52e
jofaco <jofaco1916@hotmail.com>
2024-09-20 09:12:59 -0500

    cambios en langchain para una respuesta mas continua y menos fallos

--- Chatia/views.py
+++ Chatia/views.py
@@ -33,9 +33,10 @@ db_uri = "mysql+mysqlconnector://root:vcc2022*WP@localhost:3306/trabajos"
 
 # Crear el objeto SQLDatabase usando la URI
 db = SQLDatabase.from_uri(db_uri)
+#llm = ChatOpenAI(temperature=0, model_name='gpt-4o-mini')
 llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
 #cadena = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=False)
-cadena = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=False)
+cadena = SQLDatabaseChain.from_llm(llm=llm, db=db, verbose=True)
 
 formato = """
 Data una pregunta del usuario:
@@ -45,7 +46,7 @@ Data una pregunta del usuario:
 4. Devuelve el dato en HTML organizado usando estilos Bootstrap.
 5. Si vas a mostrar datos sobre trabajos, siempre incluye un link con la siguiente dirección: https://trabajos.sccot.org/Detalle_Trabajo/idTrabajo, donde `idTrabajo` lo vas a reemplazar por el id del trabajo.
 6. Si vas a mostrar datos sobre autores, siempre incluye un link con la siguiente dirección: https://trabajos.sccot.org/Autor/detalleAutor/idAutor, donde `idAutor` lo vas a remplazar por el id del autor.
-7. Siempre que se hable de trabajos o autores incluye link.
+7. Siempre que se hable de trabajos o autores incluye link con los parametros dichos en el punto 5 y 6.
 #{question}
 """
 
@@ -65,35 +66,48 @@ class PreguntarChatGPT(LoginRequiredMixin, IsSuperuserMixin, View):
         resultado = ""
         cargando = True
         pregunta = request.POST.get('pregunta')
+        max_intentos = 1  # Número máximo de intentos
 
         if pregunta:
             consulta_sql = formato.format(question=pregunta)
-            try:
-                # Ejecuta la consulta
-                resultado = cadena.run(consulta_sql)
-                
-                # Guardar la pregunta y la respuesta en la sesión
-                preguntas = request.session.get('preguntas', [])
-                respuestas = request.session.get('respuestas', [])
-                
-                # Limitar el historial a las últimas 10 preguntas
-                if len(preguntas) >= 10:
-                    preguntas.pop(0)
-                    respuestas.pop(0)
-                
-                preguntas.append(pregunta)
-                respuestas.append(resultado)
-                
-                request.session['preguntas'] = preguntas
-                request.session['respuestas'] = respuestas
+            intentos = 0
+            
+            while intentos < max_intentos:
+                try:
+                    # Ejecuta la consulta
+                    resultado = cadena.run(consulta_sql)
+                    print(f"Resultado: {resultado}")
 
-            except Exception as e:
-                resultado = "<div class='d-flex justify-content-center'><p class='m-3 h5 text-center'>Lo sentimos, no fue posible responder la pregunta.</p></div>"
-            finally:
-                cargando = False  # Esto asegura que se actualice el estado después de procesar la consulta
+                    # Guardar la pregunta y la respuesta en la sesión
+                    preguntas = request.session.get('preguntas', [])
+                    respuestas = request.session.get('respuestas', [])
+                    
+                    # Limitar el historial a las últimas 10 preguntas
+                    if len(preguntas) >= 10:
+                        preguntas.pop(0)
+                        respuestas.pop(0)
+                    
+                    preguntas.append(pregunta)
+                    respuestas.append(resultado)
+                    
+                    request.session['preguntas'] = preguntas
+                    request.session['respuestas'] = respuestas
+                    
+                    break  # Salir del bucle si la consulta fue exitosa
+
+                except Exception as e:
+
+                    intentos += 1
+                    if intentos >= max_intentos:
+                        resultado = "<div class='d-flex justify-content-center'><p class='m-3 h5 text-center'>Lo sentimos, no fue posible responder la pregunta.<br/> intenta hacer tu pregunta mas especifica</p></div>"
+                        break  # Salir del bucle si se alcanzó el máximo de intentos
+
+                finally:
+                    cargando = False  # Actualiza el estado después de procesar la consulta
 
         context = self.get_context_data(resultado=resultado, cargando=cargando)
         return render(request, self.template_name, context)
+
 
     def get_context_data(self, **kwargs):
         preguntas = self.request.session.get('preguntas', [])
@@ -114,61 +128,3 @@ class PreguntarChatGPT(LoginRequiredMixin, IsSuperuserMixin, View):
 
 def hola_mundo(request):
     return HttpResponse('<h1>Hola Mundo</h1>')
-""" 
-class UserUpdate(LoginRequiredMixin,IsSuperuserMixin,UpdateView):
-    ''' Clase UpdateView para actualizar los usuarios. 
-
-    **Context** 
-       
-        :model:  Una instancia del modelo Usuario .
-        :form_class:  Formulario para la edición de usuarios creado en forms.py de la app usuario.
-        
-    **Methods**
-        
-        :``get_context_data(self, **kwargs)``: 
-        
-            Envio del context al formulario de editar usuarios.
-        
-        :``post(self, request, *args, **kwargs)``: 
-        
-            Recibe los datos enviados desde el formulario y realiza la validación correspondiente para realizar el update.
-    
-    **Template:**
-
-        :template_name: Formulario para la edición de un usuario.
-            
-    '''
-    model = User
-    form_class = UserForm
-    template_name = 'user_editModal.html'
-
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            pwd = form.cleaned_data['password']
-            u = form.save(commit=False)
-            user = User.objects.get(pk=u.pk)
-            if user.is_superuser:
-                messages.warning(request, 'El usuario es administrador, no se puede editar!')
-                return redirect('user:user_list')
-            else:
-                if user.password != pwd:
-                    u.set_password(pwd)
-            u.save()
-            messages.success(request, 'El usuario ha sido actualizado!')
-            return redirect('user:user_list')
-                
-        else:
-            messages.error(request, 'Error al actualizar al usuario!')
-            return redirect('user:user_list')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)        
-        context['title'] = 'Editar Usuario'
-        context['Usuarios'] = User.objects.all()        
-        return context """
